4d50b3d02415ba2bb77c9b5182c79bf0c0cca978
LiquiNaut <boetfly@gmail.com>
2024-02-29 00:14:05 +0100

    Refactored BookController to use the new BookDto class

--- Controllers/BooksController.cs
+++ Controllers/BooksController.cs
@@ -2,9 +2,11 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using BooksApi.DTOs;
 using BooksApi.Models;
 using BooksApi.UnitOfWorks;
 using Microsoft.AspNetCore.Mvc;
+
 
 namespace BooksApi.Controllers
 {
@@ -21,10 +23,25 @@ namespace BooksApi.Controllers
 
         // POST /api/books
         [HttpPost]
-        public IActionResult CreateBook(Book book)
+        public IActionResult CreateBook(BookDto bookDto)
         {
+            var binding = (BindingType)Enum.Parse(typeof(BindingType), bookDto.Binding.ToString());
+            var book = new Book
+            {
+                Title = bookDto.Title,
+                AuthorId = bookDto.AuthorId,
+                ISBN = bookDto.ISBN,
+                Language = bookDto.Language,
+                YearOfRelease = bookDto.YearOfRelease,
+                NumberOfPages = bookDto.NumberOfPages,
+                PublisherId = bookDto.PublisherId,
+                CategoryId = bookDto.CategoryId,
+                Binding = binding
+            };
+
             _unitOfWork.BookRepository.Add(book);
             _unitOfWork.SaveChanges();
+
             return CreatedAtAction(nameof(GetBookById), new { id = book.Id }, book);
         }
 
@@ -32,25 +49,54 @@ namespace BooksApi.Controllers
         [HttpGet("{id}")]
         public IActionResult GetBookById(int id)
         {
-            var book = _unitOfWork.BookRepository.GetById(id);
+
+            var book = _unitOfWork.BookRepository.GetAll()
+                .Select(b => new BookDto
+                {
+                    Id = b.Id,
+                    Title = b.Title,
+                    AuthorId = b.AuthorId,
+                    ISBN = b.ISBN,
+                    Language = b.Language,
+                    YearOfRelease = b.YearOfRelease,
+                    NumberOfPages = b.NumberOfPages,
+                    PublisherId = b.PublisherId,
+                    CategoryId = b.CategoryId,
+                    Binding = (int)b.Binding
+                })
+                .FirstOrDefault(b => b.Id == id);
+
             if (book == null)
             {
                 return NotFound();
             }
+
             return Ok(book);
         }
 
         // PUT /api/books/{id}
         [HttpPut("{id}")]
-        public IActionResult UpdateBook(int id, Book book)
+        public IActionResult UpdateBook(int id, BookDto bookDto)
         {
-            if (id != book.Id)
+            var book = _unitOfWork.BookRepository.GetById(id);
+            if (book == null)
             {
-                return BadRequest();
+                return NotFound();
             }
+
+            book.Title = bookDto.Title;
+            book.AuthorId = bookDto.AuthorId;
+            book.ISBN = bookDto.ISBN;
+            book.Language = bookDto.Language;
+            book.YearOfRelease = bookDto.YearOfRelease;
+            book.NumberOfPages = bookDto.NumberOfPages;
+            book.PublisherId = bookDto.PublisherId;
+            book.CategoryId = bookDto.CategoryId;
+            book.Binding = (BindingType)bookDto.Binding;
 
             _unitOfWork.BookRepository.Update(book);
             _unitOfWork.SaveChanges();
+
             return NoContent();
         }
 
@@ -87,9 +133,25 @@ namespace BooksApi.Controllers
             {
                 return NotFound();
             }
+
+            var book = _unitOfWork.BookRepository.GetById(loan.BookId);
+            if (book == null)
+            {
+                return NotFound();
+            }
+
             loan.State = LoanState.Returned;
             _unitOfWork.SaveChanges();
-            return NoContent();
+
+            var bookDto = new BookDto
+            {
+                Id = book.Id,
+                Title = book.Title,
+                AuthorId = book.AuthorId,
+                PublisherId = book.PublisherId,
+            };
+
+            return Ok(bookDto);
         }
     }
 }
